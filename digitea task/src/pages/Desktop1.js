ca8c455a548e54667d4da14c92a2a557fb636e6c
Anshul Soni <74638335+anshulsoni4@users.noreply.github.com>
2023-02-17 19:43:59 +0530

    Add files via upload

---
+++ digitea task/src/pages/Desktop1.js
@@ -0,0 +1,59 @@
+import "./Desktop1.css";
+
+const Desktop1 = () => {
+  return (
+    <div className="desktop-1">
+      <img className="image-1-icon" alt="" src="../image-1@2x.png" />
+      <div className="laptop-pro-2019-container">
+        <p className="laptop-pro-2019">
+          <span className="laptop-pro-20191">Laptop Pro 2019</span>
+        </p>
+        <p className="blank-line">
+          <span>
+            <span>&nbsp;</span>
+          </span>
+        </p>
+        <p className="lorem-ipsum-dolor-sit-amet-co">
+          <span>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </span>
+          </span>
+        </p>
+      </div>
+      <div className="select-specification">Select Specification</div>
+      <div className="desktop-1-child" />
+      <div className="desktop-1-item" />
+      <div className="desktop-1-inner" />
+      <div className="rectangle-div" />
+      <div className="desktop-1-child1" />
+      <div className="desktop-1-child2" />
+      <div className="silver">Silver</div>
+      <div className="space-grey">Space Grey</div>
+      <div className="gb">13.3” 512GB</div>
+      <div className="gb1">13.3” 512GB</div>
+      <div className="gb2">13.3” 512GB</div>
+      <div className="gb3">13.3” 512GB</div>
+      <div className="ghz-quad-code">
+        2.4GHZ Quad Code 8 gb RAM | 512 gb SSD
+      </div>
+      <div className="ghz-quad-code1">
+        2.4GHZ Quad Code 8 gb RAM | 512 gb SSD
+      </div>
+      <div className="ghz-quad-code2">
+        2.4GHZ Quad Code 8 gb RAM | 512 gb SSD
+      </div>
+      <div className="ghz-quad-code3">
+        2.4GHZ Quad Code 8 gb RAM | 512 gb SSD
+      </div>
+      <div className="select-color">Select Color</div>
+      <img className="ellipse-icon" alt="" src="../ellipse-1.svg" />
+      <img className="desktop-1-child3" alt="" src="../ellipse-1.svg" />
+      <img className="frame-icon" alt="" src="../frame-1@2x.png" />
+      <img className="desktop-1-child4" alt="" src="../frame-2@2x.png" />
+    </div>
+  );
+};
+
+export default Desktop1;
